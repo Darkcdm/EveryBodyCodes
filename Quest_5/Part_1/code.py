86a5dcbd2f1fd16e0fd6b593c0aa8f05136549f9
duser <daniel.knotek@ntautomation.eu>
2024-11-22 20:50:11 +0100

    some work on Quest 5

--- Quest_5/Part_1/code.py
+++ Quest_5/Part_1/code.py
@@ -1,19 +1,47 @@
 import os
 
+
+def dance():
+    pass
+
+
+
 file_path = os.path.dirname(os.path.abspath(__file__))
+DANCEROUNDS = 4
+
 
 input_string = ""
 with open(f'{file_path}/input', 'r') as file:
     input_string = file.read()
 
 
+
+
 danceFloor = []
 
 
-for y in input_string.split("\n"):
-    line = []
+for index, y in enumerate(input_string.split("\n")):
+    if len(danceFloor) <= index:
+        danceFloor.append([])
     for x in y.split():
-        line.append(int(x))
+        danceFloor[index].append(int(x))
         
-    danceFloor.append(line)
-    
+
+
+dancerIndex = 0
+for round in range(DANCEROUNDS):
+    if dancerIndex >= len(danceFloor):
+        dancerIndex = 0
+
+    dancerCollum = danceFloor[dancerIndex]
+    dancer = dancerCollum[0]
+    newDancerCollum = danceFloor[-len(danceFloor) + 1 + dancerIndex]
+
+
+    div =  len(newDancerCollum) / dancer
+    mod =  dancer % len(newDancerCollum) 
+
+    print(f"Dancer:{dancer} CollLen:{len(newDancerCollum)} Div: {div} Mod: {mod}")
+
+
+    dancerIndex += 1
